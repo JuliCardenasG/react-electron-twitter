7851777640774bd7662464cea5071167af86a33c
Julián <julicardenasgonzalez@gmail.com>
2020-11-20 17:00:59 +0100

    Fix linter issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
   rules: {
     // A temporary hack related to IDE not resolving correct package.json
     'import/no-extraneous-dependencies': 'off',
-    'semicolon': [true, 'always', 'ignore-bound-class-methods'],
   },
   parserOptions: {
     ecmaVersion: 2020,
